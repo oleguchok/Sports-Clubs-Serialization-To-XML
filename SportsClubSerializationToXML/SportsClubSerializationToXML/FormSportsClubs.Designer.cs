8beecd08adcd3345766612a7237118be3b9dcca6
oleguhcok <oleguchok@gmail.com>
2015-04-03 23:30:10 +0300

    Adding add club functionality on form

--- SportsClubSerializationToXML/SportsClubSerializationToXML/FormSportsClubs.Designer.cs
+++ SportsClubSerializationToXML/SportsClubSerializationToXML/FormSportsClubs.Designer.cs
@@ -29,23 +29,324 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButtonFootball = new System.Windows.Forms.RadioButton();
+            this.radioButtonBasketball = new System.Windows.Forms.RadioButton();
+            this.radioButtonVolleyball = new System.Windows.Forms.RadioButton();
+            this.radioButtonHockey = new System.Windows.Forms.RadioButton();
+            this.radioButtonRugby = new System.Windows.Forms.RadioButton();
+            this.radioButtonHandball = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(337, 57);
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
             this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(177, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 136);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(177, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 175);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(177, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name of Club";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Country";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Home Arena";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Coach";
+            // 
+            // radioButtonFootball
+            // 
+            this.radioButtonFootball.AutoSize = true;
+            this.radioButtonFootball.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonFootball.Name = "radioButtonFootball";
+            this.radioButtonFootball.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonFootball.TabIndex = 10;
+            this.radioButtonFootball.TabStop = true;
+            this.radioButtonFootball.Text = "Football";
+            this.radioButtonFootball.UseVisualStyleBackColor = true;
+            this.radioButtonFootball.CheckedChanged += new System.EventHandler(this.radioButtonFootball_CheckedChanged);
+            // 
+            // radioButtonBasketball
+            // 
+            this.radioButtonBasketball.AutoSize = true;
+            this.radioButtonBasketball.Location = new System.Drawing.Point(97, 19);
+            this.radioButtonBasketball.Name = "radioButtonBasketball";
+            this.radioButtonBasketball.Size = new System.Drawing.Size(74, 17);
+            this.radioButtonBasketball.TabIndex = 11;
+            this.radioButtonBasketball.TabStop = true;
+            this.radioButtonBasketball.Text = "Basketball";
+            this.radioButtonBasketball.UseVisualStyleBackColor = true;
+            this.radioButtonBasketball.CheckedChanged += new System.EventHandler(this.radioButtonBasketball_CheckedChanged);
+            // 
+            // radioButtonVolleyball
+            // 
+            this.radioButtonVolleyball.AutoSize = true;
+            this.radioButtonVolleyball.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonVolleyball.Name = "radioButtonVolleyball";
+            this.radioButtonVolleyball.Size = new System.Drawing.Size(69, 17);
+            this.radioButtonVolleyball.TabIndex = 12;
+            this.radioButtonVolleyball.TabStop = true;
+            this.radioButtonVolleyball.Text = "Volleyball";
+            this.radioButtonVolleyball.UseVisualStyleBackColor = true;
+            this.radioButtonVolleyball.CheckedChanged += new System.EventHandler(this.radioButtonVolleyball_CheckedChanged);
+            // 
+            // radioButtonHockey
+            // 
+            this.radioButtonHockey.AutoSize = true;
+            this.radioButtonHockey.Location = new System.Drawing.Point(97, 42);
+            this.radioButtonHockey.Name = "radioButtonHockey";
+            this.radioButtonHockey.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonHockey.TabIndex = 13;
+            this.radioButtonHockey.TabStop = true;
+            this.radioButtonHockey.Text = "Hockey";
+            this.radioButtonHockey.UseVisualStyleBackColor = true;
+            this.radioButtonHockey.CheckedChanged += new System.EventHandler(this.radioButtonHockey_CheckedChanged);
+            // 
+            // radioButtonRugby
+            // 
+            this.radioButtonRugby.AutoSize = true;
+            this.radioButtonRugby.Location = new System.Drawing.Point(6, 65);
+            this.radioButtonRugby.Name = "radioButtonRugby";
+            this.radioButtonRugby.Size = new System.Drawing.Size(56, 17);
+            this.radioButtonRugby.TabIndex = 14;
+            this.radioButtonRugby.TabStop = true;
+            this.radioButtonRugby.Text = "Rugby";
+            this.radioButtonRugby.UseVisualStyleBackColor = true;
+            this.radioButtonRugby.CheckedChanged += new System.EventHandler(this.radioButtonRugby_CheckedChanged);
+            // 
+            // radioButtonHandball
+            // 
+            this.radioButtonHandball.AutoSize = true;
+            this.radioButtonHandball.Location = new System.Drawing.Point(97, 65);
+            this.radioButtonHandball.Name = "radioButtonHandball";
+            this.radioButtonHandball.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonHandball.TabIndex = 15;
+            this.radioButtonHandball.TabStop = true;
+            this.radioButtonHandball.Text = "Handball";
+            this.radioButtonHandball.UseVisualStyleBackColor = true;
+            this.radioButtonHandball.CheckedChanged += new System.EventHandler(this.radioButtonHandball_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFootball);
+            this.groupBox1.Controls.Add(this.radioButtonHandball);
+            this.groupBox1.Controls.Add(this.radioButtonBasketball);
+            this.groupBox1.Controls.Add(this.radioButtonRugby);
+            this.groupBox1.Controls.Add(this.radioButtonVolleyball);
+            this.groupBox1.Controls.Add(this.radioButtonHockey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 201);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 89);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kinds of sports";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 332);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 371);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 410);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 449);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(12, 310);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(174, 20);
+            this.textBox7.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(12, 348);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(174, 20);
+            this.textBox8.TabIndex = 24;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(12, 387);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(174, 20);
+            this.textBox9.TabIndex = 25;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(12, 426);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(174, 20);
+            this.textBox10.TabIndex = 26;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Enabled = false;
+            this.textBox11.Location = new System.Drawing.Point(12, 465);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(174, 20);
+            this.textBox11.TabIndex = 27;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 491);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(87, 23);
+            this.buttonAdd.TabIndex = 28;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // FormSportsClubs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 480);
+            this.ClientSize = new System.Drawing.Size(821, 518);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "FormSportsClubs";
             this.Text = "Sports Clubs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +355,33 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButtonFootball;
+        private System.Windows.Forms.RadioButton radioButtonBasketball;
+        private System.Windows.Forms.RadioButton radioButtonVolleyball;
+        private System.Windows.Forms.RadioButton radioButtonHockey;
+        private System.Windows.Forms.RadioButton radioButtonRugby;
+        private System.Windows.Forms.RadioButton radioButtonHandball;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
 
--- SportsClubSerializationToXML/SportsClubSerializationToXML/FormSportsClubs.cs
+++ SportsClubSerializationToXML/SportsClubSerializationToXML/FormSportsClubs.cs
@@ -13,12 +13,92 @@ namespace SportsClubSerializationToXML
 {
     public partial class FormSportsClubs : Form
     {
+        SportsClubs club;
+
         public FormSportsClubs()
         {
             InitializeComponent();
-            FootballClub f = new FootballClub();
-            f.Name = "ss";
-            textBox1.Text = f.ToString();
         }
+
+        private void TextBoxEnabled()
+        {
+            textBox7.Enabled = true;
+            textBox8.Enabled = true;
+            textBox9.Enabled = true;
+            textBox10.Enabled = true;
+            textBox11.Enabled = true;
+        }
+
+        private void radioButtonFootball_CheckedChanged(object sender, EventArgs e)
+        {
+
+            TextBoxEnabled();
+            label7.Text = "Count of Goalkeepers";
+            label8.Text = "Count of Defenders";
+            label9.Text = "Count of Midfielders";
+            label10.Text = "Count of Forwards";
+            textBox11.Enabled = false;
+            label11.Text = "";
+        }
+
+        private void radioButtonBasketball_CheckedChanged(object sender, EventArgs e)
+        {
+            TextBoxEnabled();
+            label7.Text = "Count of Point Guards";
+            label8.Text = "Count of Shooting Guards";
+            label9.Text = "Count of Small Forwards";
+            label10.Text = "Count of Power Forwards";
+            label11.Text = "Count of Centre";
+        }
+
+        private void radioButtonVolleyball_CheckedChanged(object sender, EventArgs e)
+        {
+            TextBoxEnabled();
+            label7.Text = "Count of Middle Blockers";
+            label8.Text = "Count of Outside Hitters";
+            label9.Text = "Count of Libero";
+            label10.Text = "Count of Setters";
+            label11.Text = "Count of Opposite";
+        }
+
+        private void radioButtonHockey_CheckedChanged(object sender, EventArgs e)
+        {
+            TextBoxEnabled();
+            label7.Text = "Count of Goalkeepers";
+            label8.Text = "Count of Defenders";
+            label9.Text = "Count of Left Forwards";
+            label10.Text = "Count of Center Forwards";
+            label11.Text = "Count of Right Forwards";
+        }
+
+        private void radioButtonRugby_CheckedChanged(object sender, EventArgs e)
+        {
+            TextBoxEnabled();
+            label7.Text = "Count of Defenders";
+            label8.Text = "Count of Attackers";
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+            textBox10.Enabled = false;
+            textBox11.Enabled = false;
+            textBox9.Enabled = false;
+        }
+
+        private void radioButtonHandball_CheckedChanged(object sender, EventArgs e)
+        {
+            TextBoxEnabled();
+            label7.Text = "Count of Goalkeepers";
+            label8.Text = "Count of Wingers";
+            label9.Text = "Count of Backs";
+            label10.Text = "Count of Centre";
+            label11.Text = "Count of Circle Runner";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
